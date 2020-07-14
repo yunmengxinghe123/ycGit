93ffaa6794e8c9e3a19e321e24940d9793e8728f
ycsendfood <yc18310801724@outlook.com>
2020-07-14 17:42:06 +0800

    主页面

---
+++ app/src/main/java/com/yc/sendfood/InfoActivity.java
@@ -0,0 +1,18 @@
+package com.yc.sendfood;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Created by Garrylin on 2020/7/14.
+ */
+public class InfoActivity extends Activity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+}
--- app/src/main/java/com/yc/sendfood/UserActivity.java
+++
@@ -1,7 +0,0 @@
-package com.yc.sendfood;
-
-/**
- * Created by Garrylin on 2020/7/14.
- */
-public class UserActivity {
-}
