a91d4313539ae52bc445037d0f550260c85d1363
ycsendfood <yc18310801724@outlook.com>
2020-07-15 09:40:05 +0800

    主页面

--- app/src/main/java/com/yc/sendfood/InfoActivity.java
+++ app/src/main/java/com/yc/sendfood/InfoActivity.java
@@ -13,6 +13,8 @@ public class InfoActivity extends Activity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_info);
 
     }
+
 }
